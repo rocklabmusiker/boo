7ebbc021cb6a3d3c88b9d5648a60e54770fb70e2
rocklabmusiker <rocklabmusiker@gmail.com>
2016-11-01 15:32:01 +0100

    change to gulp

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+//= libs/init.js
+//= libs/jquery.arcticmodal.min.js
+//= libs/jquery.bxslider.min.js
+//= libs/jquery.fancybox.pack.js
+//= libs/jquery.maskedinput.min.js
+//= libs/jquery.touchswipe.min.js
+//= libs/jquery-ui.min.js
+//= libs/map.js
+
+
+//= libs/app.js
+
+
+
